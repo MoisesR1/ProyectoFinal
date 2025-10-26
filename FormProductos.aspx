aed84fe3f97cb63b7d3d1bd15adb0255cd2e6c74
mrlop <mrlop@DELL-S>
2025-10-25 22:24:13 -0600

    Agregar gridview y base datos

---
+++ FormProductos.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="FormProductos.aspx.vb" Inherits="ProyectoFinal.FormProductos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+     <asp:hiddenField ID="editando" runat="server" />
+     <h2>Inventario de Ferreteria Brenes</h2>
+
+    <asp:TextBox ID="Txt_id" Placeholder="ID" runat="server"></asp:TextBox>
+    <asp:TextBox ID="Txt_descripcion" Placeholder="Descripcion" runat="server"></asp:TextBox>
+    <asp:TextBox ID="Txt_precio" Placeholder="Precio" runat="server"></asp:TextBox>
+    <asp:TextBox ID="Txt_cantidad" Placeholder="Cantidad" runat="server"></asp:TextBox>
+    <asp:Button ID="Btn_Agregar" Text="Agregar Producto" runat="server" OnClick="Btn_Agregar_Click" />
+    <asp:Label ID="lblMensaje" runat="server" Text=""></asp:Label>
+    <br />
+    <asp:GridView ID="gvProductos" runat="server" AutoGenerateColumns="False" DataKeyNames="ProductoID" OnRowEditing="gvProductos_RowEditing" OnRowCancelingEdit="gvProductos_RowCancelingEdit" OnRowUpdating="gvProductos_RowUpdating" OnRowDeleting="gvProductos_RowDeleting">
+        <Columns>
+            <asp:BoundField DataField="ProductoID" HeaderText="ID" ReadOnly="True" />
+            <asp:BoundField DataField="Descripcion" HeaderText="Descripcion" />
+            <asp:BoundField DataField="Precio" HeaderText="Precio" DataFormatString="{0:C}" />
+            <asp:BoundField DataField="Cantidad" HeaderText="Cantidad" />
+            <asp:CommandField ShowEditButton="True" ShowDeleteButton="True" />
+        </Columns>
+        <asp:SqlDataSource ID="SqlDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:II-46ConnectionString %>" ProviderName="<%$ ConnectionStrings:II-46ConnectionString.ProviderName %>" SelectCommand="SELECT * FROM [Productos]"></asp:SqlDataSource>
+    </asp:GridView>
+</asp:Content>
---
+++ FormProductos.aspx.designer.vb
@@ -0,0 +1,96 @@
+﻿'------------------------------------------------------------------------------
+' <generado automáticamente>
+'     Este código fue generado por una herramienta.
+'
+'     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+'     se vuelve a generar el código. 
+' </generado automáticamente>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class FormProductos
+
+    '''<summary>
+    '''Control editando.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents editando As Global.System.Web.UI.WebControls.HiddenField
+
+    '''<summary>
+    '''Control Txt_id.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Txt_id As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Txt_descripcion.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Txt_descripcion As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Txt_precio.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Txt_precio As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Txt_cantidad.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Txt_cantidad As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control Btn_Agregar.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents Btn_Agregar As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Control lblMensaje.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents lblMensaje As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control gvProductos.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents gvProductos As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control SqlDataSource.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents SqlDataSource As Global.System.Web.UI.WebControls.SqlDataSource
+End Class
